af166c5bbd71d378f91f3416e4e018de432defb7
TANISH SINGH TAK <78207332+TanishSinghTak@users.noreply.github.com>
2024-02-06 22:25:39 +0530

    Add files via upload

--- e7.cpp
+++ e7.cpp
@@ -4,8 +4,8 @@
 #include <ctime>
 // Program with perspective correct interpolation of vertex attributes.
 
-namespace R = COL781::Software;
-// namespace R = COL781::Hardware;
+// namespace R = COL781::Software;
+namespace R = COL781::Hardware;
 using namespace glm;
 
 
@@ -57,26 +57,18 @@ class Camera{
         vec3 UpVec;
 };
 
-void createWall (R::Rasterizer &r, R::ShaderProgram &program, R::Object &shape, mat4 view, mat4 projection, int x, int y, int z, int angle) {
-
-    mat4 wallScale = scale(mat4(1.0f), vec3(1.0f, 1.0f, 1.0f));
-    mat4 wallRotate = rotate(mat4(1.0f), radians(0.0f + angle), vec3(0.0f,1.0f,0.0f));
-    mat4 wallModel = translate(mat4(1.0f), vec3(0.0f+x,0.0f+y, 0.0f+z));
-    r.setUniform(program, "transform", projection * view * wallModel * wallRotate * wallScale);
-    r.drawObject(shape);
-}
-
-void createRoad (R::Rasterizer &r, R::ShaderProgram &program, R::Object &shape, mat4 view, mat4 projection, int x, int y) {
+void createGround (R::Rasterizer &r, R::ShaderProgram &program, R::Object &shape, mat4 view, mat4 projection, float Scale) {
     
-    mat4 roadScale = scale(mat4(1.0f), vec3(1.0f, 1.0f, 1.0f));
-    mat4 roadModel = translate(mat4(1.0f), vec3(0.0f + x, -3.0f, -0.5f+y));
-    r.setUniform(program, "transform", projection * view * roadModel * roadScale);
+    mat4 groundScale = scale(mat4(1.0f), vec3(Scale, Scale, Scale));
+    mat4 groundModel = translate(mat4(1.0f), vec3(0.0f, -3.0f, -0.5f));
+    r.setUniform(program, "transform", projection * view * groundModel * groundScale);
     r.drawObject(shape);
 
-    mat4 roadModel2 = translate(mat4(1.0f), vec3(0.0f + x, -3.0f, 0.5f+y));
-    r.setUniform(program, "transform", projection * view * roadModel2 * roadScale);
+    mat4 groundModel2 = translate(mat4(1.0f), vec3(0.0f , -3.0f, 0.5f));
+    r.setUniform(program, "transform", projection * view * groundModel2 * groundScale);
     r.drawObject(shape);
 }
+
 void createPerson (R::Rasterizer &r, R::ShaderProgram &program, R::Object &shape, mat4 view, mat4 projection) {
     
         mat4 headScale = translate(mat4(1.0f), vec3(1.7f, 0.5f, 0.0f));
@@ -114,6 +106,21 @@ void createPerson (R::Rasterizer &r, R::ShaderProgram &program, R::Object &shape
         mat4 RightLegModel = translate(mat4(1.0f), vec3(1.7f, -1.7f, -0.25f));
         r.setUniform(program, "transform", projection * view * RightLegModel * RightLegRotatation * LegTranslateDown * LegScale);
         r.drawObject(shape);
+}
+
+void createFlag(R::Rasterizer &r, R::ShaderProgram &program, R::Object &flagPole, R::Object &Flag, mat4 view, mat4 projection){
+        
+        float time = SDL_GetTicks()*1e-3;
+        mat4 flagPoleScale = scale(mat4(1.0f), vec3(0.3f, 5.5f, 0.3f));
+        mat4 flagPoleModel = translate(mat4(1.0f), vec3(0.0f, -3.0f, 0.0f));
+        r.setUniform(program, "transform", projection * view * flagPoleModel * flagPoleScale);
+        r.drawObject(flagPole);
+
+        mat4 flagScale = scale(mat4(1.0f), vec3(1.4f, 1.0f, 1.0f));
+        mat4 flagModel = translate(mat4(1.0f), vec3(-2.3f, 6.3f, 0.0f));
+        mat4 flagRotate = rotate(mat4(1.0f), radians(time*40.0f), vec3(0.0f,1.0f,0.0f));
+        r.setUniform(program, "transform", projection * view * flagRotate * flagModel * flagScale);
+        r.drawObject(Flag);
 }
 
 int main() {
@@ -168,13 +175,13 @@ int main() {
 		r.setTriangleIndices(cube, 12, triangles);
 	}
 	
-    R::Object roadTiles = r.createObject();
+    R::Object Ground = r.createObject();
     {
         vec4 vertices[] = {
-            vec4( -0.5,  0.0,  0.5, 1.0),
-            vec4(  0.5,  0.0,  0.5, 1.0),
-            vec4( -0.5,  0.0, -0.5, 1.0),
-            vec4(  0.5,  0.0, -0.5, 1.0)
+            vec4( -5.0,  0.0,  5.0, 1.0),
+            vec4(  5.0,  0.0,  5.0, 1.0),
+            vec4( -5.0,  0.0, -5.0, 1.0),
+            vec4(  5.0,  0.0, -5.0, 1.0)
         };
         ivec3 triangles[] = {
             ivec3(0, 1, 2),
@@ -187,9 +194,9 @@ int main() {
             vec4(0.0, 1.0, 0.0, 1.0)
         };
 
-        r.setVertexAttribs(roadTiles, 0, 4, vertices);   
-        r.setVertexAttribs(roadTiles, 1, 4, colors);
-        r.setTriangleIndices(roadTiles, 2, triangles);
+        r.setVertexAttribs(Ground, 0, 4, vertices);   
+        r.setVertexAttribs(Ground, 1, 4, colors);
+        r.setTriangleIndices(Ground, 2, triangles);
     }
 
     R::Object flagPole = r.createObject();
@@ -231,9 +238,9 @@ int main() {
             ivec3(0,10,6)
         };
         vec4 colors[] = {
-            vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0),
-            vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0),
-            vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0)
+            vec4(0.0, 0.0, 0.0, 1.0), vec4(0.0, 0.0, 0.0, 1.0), vec4(0.0, 0.0, 0.0, 1.0), vec4(0.0, 0.0, 0.0, 1.0),
+            vec4(0.0, 0.0, 0.0, 1.0), vec4(0.0, 0.0, 0.0, 1.0), vec4(0.3765, 0.5098, 0.7137, 1.0),vec4(0.3765, 0.5098, 0.7137, 1.0),
+            vec4(0.3765, 0.5098, 0.7137, 1.0),vec4(0.3765, 0.5098, 0.7137, 1.0), vec4(0.3765, 0.5098, 0.7137, 1.0),vec4(0.3765, 0.5098, 0.7137, 1.0)
         };
         r.setVertexAttribs(flagPole, 0, 12, vertices);
         r.setVertexAttribs(flagPole, 1, 12, colors);
@@ -241,30 +248,6 @@ int main() {
     }
 
     
-    R::Object wallTiles = r.createObject();
-    {
-        vec4 vertices[] = {
-            vec4( -0.5,  0.5, 0.0, 1.0),
-            vec4(  0.5, -0.5, 0.0, 1.0),
-            vec4( -0.5, -0.5, 0.0, 1.0),
-            vec4(  0.5,  0.5, 0.0, 1.0)
-        };
-        ivec3 triangles[] = {
-            ivec3(0, 1, 2),
-            ivec3(1, 0, 3)
-        };
-        vec4 colors[] = {
-            vec4(0.0, 0.0, 1.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0)
-        };
-
-        r.setVertexAttribs(wallTiles, 0, 4, vertices);   
-        r.setVertexAttribs(wallTiles, 1, 4, colors);
-        r.setTriangleIndices(wallTiles, 2, triangles);
-    }
-
     R::Object Flag = r.createObject();{
         vec4 vertices[] = {
             vec4( 1.5,  0.2, 0.0, 1.0),
@@ -304,67 +287,33 @@ int main() {
     myCamera.moveRight(-1.5f);
     
     mat4 projection = perspective(radians(60.0f), (float)width/(float)height, 0.1f, 100.0f);
-    float totalAngle = 0.0;
-    float f = 1.0;
+
     while (!r.shouldQuit()) {
-        r.clear(vec4(1.0, 1.0, 1.0, 1.0));
+        r.clear(vec4(0.0, 0.0, 1.0, 1.0));
         r.useShaderProgram(program);
-        float time = SDL_GetTicks()*1e-3;
+        
 
         mat4 view = myCamera.getViewMatrix();
         
         createPerson(r, program, cube, view, projection);
-        for(int i =0; i<=30; i++){
-            for(int j =0; j<=30; j++){
-                createRoad(r, program, roadTiles, view, projection, j, i);
-                createRoad(r, program, roadTiles, view, projection,-j, i);
-                createRoad(r, program, roadTiles, view, projection, j, -i);
-                createRoad(r, program, roadTiles, view, projection,-j, -i);
-            }
-        }
 
-        mat4 flagPoleScale = scale(mat4(1.0f), vec3(0.3f, 5.5f, 0.3f));
-        mat4 flagPoleModel = translate(mat4(1.0f), vec3(0.0f, -3.0f, 0.0f));
-        r.setUniform(program, "transform", projection * view * flagPoleModel * flagPoleScale);
-        r.drawObject(flagPole);
+        createGround(r, program, Ground, view, projection,1.5);
 
-        mat4 flagScale = scale(mat4(1.0f), vec3(1.4f, 1.0f, 1.0f));
-        mat4 flagModel = translate(mat4(1.0f), vec3(-2.3f, 6.3f, 0.0f));
-        mat4 flagRotate;
-        
-        
-        flagRotate = rotate(mat4(1.0f), f*radians(time*40.0f), vec3(0.0f,1.0f,0.0f));
-        r.setUniform(program, "transform", projection * view * flagRotate * flagModel * flagScale);
-        r.drawObject(Flag);
-
-        
-
-        for(int i = -3; i<=50; i++){
-            for(int j =0; j<=50; j++){
-                createWall(r, program, wallTiles, view, projection,  j, i, 30, 0);
-                createWall(r, program, wallTiles, view, projection, -j, i, 30, 0);
-                createWall(r, program, wallTiles, view, projection,  j, i,-30, 0);
-                createWall(r, program, wallTiles, view, projection, -j, i,-30, 0);
-                createWall(r, program, wallTiles, view, projection,  30, i, j, 90);
-                createWall(r, program, wallTiles, view, projection,  30, i,-j, 90);
-                createWall(r, program, wallTiles, view, projection, -30, i, j, 90);
-                createWall(r, program, wallTiles, view, projection, -30, i,-j, 90);
-            }
-        }
+        createFlag(r, program, flagPole, Flag, view, projection);
 
         std::time_t currentTime = std::time(nullptr);
         std::tm* localTime = std::localtime(&currentTime);
         int second = localTime->tm_sec;
         second %= 6;
         if(second <3){
-            myCamera.moveForward(0.1f);
-            myCamera.moveRight(0.5f);
-            myCamera.rotateRight(5.0f);
+            myCamera.moveForward(0.01f);
+            myCamera.moveRight(0.3f);
+            myCamera.rotateRight(3.0f);
         }
         else{
-            myCamera.moveForward(-0.1f);
-            myCamera.moveRight(-0.5f);
-            myCamera.rotateRight(-5.0f);
+            myCamera.moveForward(-0.01f);
+            myCamera.moveRight(-0.3f);
+            myCamera.rotateRight(-3.0f);
         }
 
         r.show();
